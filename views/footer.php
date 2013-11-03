f6c5964ba06548f78cad92c8ba6fdba27d865b53
Jeremy Katras <jkatras@fullsail.edu>
2013-11-03 17:08:06 -0500

    moving script tags to footer view

--- views/footer.php
+++ views/footer.php
@@ -2,3 +2,9 @@
 
 
 ?>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js" type="text/javascript"></script>
+	<script src="controllers/mainController.js" type="text/javascript"></script>
+	<!--<script src="libraries/angular.min.js" type="text/javascript"></script>-->
+	<link rel="stylesheet" href="assets/Bootstraps/html/css/bootstrap.css"/>
+</body>
