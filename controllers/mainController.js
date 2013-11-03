14f53c7ea7339a8b36bd93a83d86542d26554654
Jeremy Katras <jkatras@fullsail.edu>
2013-11-03 16:25:56 -0500

    [#59664868] Removed curlybraces inside $http params, array is returned in console

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,11 @@
-function mainController($scope, $http){
+function mainController($scope, $http) {
  	
  	$scope.appId = "4606347e";
     $scope.apiKey = "2b0dc330fcebb3d65bdddc74aae878b3";
     $scope.init = function() {
       
      	//URL parameters are hard-coded to 'onion soup' for testing purposes
-        $http.jsonp({'http://api.yummly.com/v1/api/recipes?_app_id='+$scope.appId+'&_app_key='+$scope.apiKey+'&q=onion+soup'+'&callback=JSON_CALLBACK'}).//added 'headers' b/c of 405 error
+        $http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=onion+soup' + '&callback=JSON_CALLBACK').//added 'headers' b/c of 405 error
         	success(function(data) {
             	console.log(data);
         	}).
