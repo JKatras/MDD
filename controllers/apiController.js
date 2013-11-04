1033d57539ffad0dfc958eda152b35d5d118c022
Jeremy Katras <jkatras@fullsail.edu>
2013-11-04 13:26:31 -0500

    [#59664866] Base File Structure is setup
    Still need to find best way to incorporate views

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -13,14 +13,14 @@ app.controller("apiController", function ($scope, $http) {
         $http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=onion+soup' + '&callback=JSON_CALLBACK').
         	//if successful connection, data console logged
         	success(function(data) {
-        		//console.log(data);
+        		console.log(data);
         //    	console.log(data.matches[1].recipeName);
-	        	angular.forEach(data, function(value, index) {
-	        		var matches = data.matches;
-	        		console.log(matches);
+//	        	angular.forEach(data, function(value, index) {
+//	        		var matches = data.matches;
+//	        		console.log(matches);
 //	        		$scope.results.push(recipe);
 //	        		console.log($scope.results);
-        		}); //angular.forEach
+//        		}); //angular.forEach
         	}).error(function(error) {
 				console.log('Could not retrieve data');
 
