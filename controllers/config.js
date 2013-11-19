ff0497aed8ca95a6393c979b65b73a01861f0926
Jeremy Katras <jkatras@fullsail.edu>
2013-11-19 12:20:17 -0500

    Replaced views/controllers to incorporate UI-Router (see description)
    -Added UI-Router to /libraries
    -Nested ui-views in index/search view/search.results/search.details
    -controllers combined into config.js
    -some tweaks to media to improve form-group padding at small sizes

--- controllers/config.js
+++ controllers/config.js
@@ -1,22 +1,94 @@
-var EatInApp = angular.module('EatInApp', ['appControllers']);
+var EatInApp = angular.module('EatInApp', ["ui.router"]);
 
-EatInApp.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl:'views/search.html'
-	//	controller:'searchCtrl'
-	//controller never works, has to be commented out
-	}).
-	when('/details/:id', {
-		templateUrl: 'views/details.html',
-		controller: 'detailCtrl'
-	}).
-	when('/login', {
-		templateUrl: 'views/login.html'
-	}).
-	when('/signup', {
-		templateUrl: 'views/signup.html'
-	}).
-	otherwise({
-		redirectTo: '/'
-	});
-}]);
+
+EatInApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/search");
+	$stateProvider
+	.state('search', {
+		url: '/search',
+		templateUrl: 'views/search.html'
+//		controller: function ($scope, $http) {
+//		 	//var for unique App ID
+//		 	$scope.appId = '4606347e';
+//		 	//var for unique API Key
+//		    $scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+//		    //array that will contain search results
+//		    $scope.results = [];
+//	     	//results returned will be JSONP format
+//	        $http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=' + $scope.keyword + '&allowedIngredient=' + $scope.include + '&excludedIngredient=' + $scope.exclude + '&requirePictures=true&callback=JSON_CALLBACK').
+//	        	//if successful return, parse data
+//		        success(function(data) {
+//		        	console.log(data); // **uncomment to view all returned data**
+//		        	//forEach loop runs through matches, var assigned to recipeNames
+//		        	//push names to results array
+//		        	angular.forEach(data.matches, function(recipe, index) {
+//		        		$scope.results.push(recipe);
+//		    		});
+//		        }). // success
+//		    	error(function(error) {
+//					//message in case of request error
+//				}); // error
+//		} 
+	})
+	.state('search.results', {
+		url: '/results',
+		templateUrl: 'views/search.results.html',
+		controller: function ($scope, $http) {
+		 	//var for unique App ID
+		 	$scope.appId = '4606347e';
+		 	//var for unique API Key
+		    $scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+		    //array that will contain search results
+		    $scope.results = [];
+	     	//results returned will be JSONP format
+	        $http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=' + $scope.keyword + '&allowedIngredient=' + $scope.include + '&excludedIngredient=' + $scope.exclude + '&requirePictures=true&callback=JSON_CALLBACK').
+        	//if successful return, parse data
+	        success(function(data) {
+	        	console.log(data); // **uncomment to view all returned data**
+	        	//forEach loop runs through matches, var assigned to recipeNames
+	        	//push names to results array
+	        	angular.forEach(data.matches, function(recipe, index) {
+	        		$scope.results.push(recipe);
+	    		});
+	        }). // success
+	    	error(function(error) {
+				//message in case of request error
+			}); // error
+		} 
+	})
+	.state('search.details', {
+		url: '/details/:id',
+		templateUrl: 'views/search.details.html',
+		controller: function ($scope, $http, $stateParams) {
+			$scope.id = $stateParams.id;
+//			$scope.details = [];
+			$scope.appId = '4606347e';
+    		$scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
+	    	$http.jsonp('http://api.yummly.com/v1/api/recipe/' +$stateParams.id
+	    	+ '?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&callback=JSON_CALLBACK').
+	    	success(function(data) {
+	    		$scope.details = data;
+	    		console.log($scope.details);
+//				angular.forEach(data, function(detail, index) {
+//					if (typeof data == "object") {
+//						$scope.details.push(detail);
+//					}
+//	    		});
+	    	//	console.log($scope.details);
+	    	}). //success
+	    	error(function(error) {
+				console.log('Recipe not found');
+			}); // error
+			
+	   	}
+	})
+//	when('/login', {
+//		templateUrl: 'views/login.html'
+//	}).
+//	when('/signup', {
+//		templateUrl: 'views/signup.html'
+//	}).
+//	otherwise({
+//		redirectTo: '/'
+//	});
+});
