34290e8798090ab2f1a2c53b6cb556d053bee57d
Jeremy Katras <jkatras@fullsail.edu>
2013-11-19 14:50:56 -0500

    removed old views to Ver 1 folder. Detail img now centers at sm size

--- controllers/config.js
+++ controllers/config.js
@@ -7,28 +7,14 @@ EatInApp.config(function($stateProvider, $urlRouterProvider) {
 	.state('search', {
 		url: '/search',
 		templateUrl: 'views/search.html'
-//		controller: function ($scope, $http) {
-//		 	//var for unique App ID
-//		 	$scope.appId = '4606347e';
-//		 	//var for unique API Key
-//		    $scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
-//		    //array that will contain search results
-//		    $scope.results = [];
-//	     	//results returned will be JSONP format
-//	        $http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=' + $scope.keyword + '&allowedIngredient=' + $scope.include + '&excludedIngredient=' + $scope.exclude + '&requirePictures=true&callback=JSON_CALLBACK').
-//	        	//if successful return, parse data
-//		        success(function(data) {
-//		        	console.log(data); // **uncomment to view all returned data**
-//		        	//forEach loop runs through matches, var assigned to recipeNames
-//		        	//push names to results array
-//		        	angular.forEach(data.matches, function(recipe, index) {
-//		        		$scope.results.push(recipe);
-//		    		});
-//		        }). // success
-//		    	error(function(error) {
-//					//message in case of request error
-//				}); // error
-//		} 
+	})
+	.state('login', {
+		url: '/login',
+		templateUrl: 'views/login.html'
+	})
+	.state('signup', {
+		url: '/signup',
+		templateUrl: 'views/signup.html'
 	})
 	.state('search.results', {
 		url: '/results',
