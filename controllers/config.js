9618c19c0223bb4316a4506ad98390f899fc1f80
Jeremy Katras <jkatras@fullsail.edu>
2015-04-11 12:24:29 -0400

    reloadOnSearch:false
    doesn’t seem to make a difference in updating $scope in results view

--- controllers/config.js
+++ controllers/config.js
@@ -6,6 +6,7 @@ EatInApp.config(['$routeProvider',
 			{
 				templateUrl: 'views/search.html',
 				controller: 'ApiCtrl'
+//				reloadOnSearch: false
 			}
 		)
 		.when('/about',
@@ -16,8 +17,9 @@ EatInApp.config(['$routeProvider',
 		)
 		.when('/search/results',
 			{
-				templateUrl: 'views/results.html',
-				controller: 'ApiCtrl'
+				templateUrl: 'views/results.html'
+//				reloadOnSearch: false
+//				controller: 'ApiCtrl'
 			}
 		)
 		.otherwise({
@@ -29,8 +31,7 @@ EatInApp.controller('AppCtrl', function ($scope, $route, $location) {
 	$scope.current = $location.path();
 });
 
-EatInApp.controller('ApiCtrl', function ($scope, $http) {
-	var resultList = this;
+EatInApp.controller('ApiCtrl', function ($scope, $http, $location) {
 	$scope.results = [];
 	$scope.appId = '4606347e';
 	$scope.apiKey = '2b0dc330fcebb3d65bdddc74aae878b3';
@@ -44,9 +45,11 @@ EatInApp.controller('ApiCtrl', function ($scope, $http) {
 				
 				$http.jsonp('http://api.yummly.com/v1/api/recipes?_app_id=' + $scope.appId + '&_app_key=' + $scope.apiKey + '&q=' + $scope.keyword + '&allowedIngredient[]=' + $scope.include + '&excludedIngredient[]=' + $scope.exclude + '&requirePictures=true&callback=JSON_CALLBACK').
 			  success(function(data) {
+//			  	$location.path('/search/results'); 
 			  	angular.forEach(data.matches, function(recipe, index) {
 			  		$scope.results.push(recipe);
 					});
+					$location.path('search/results'); 
 					console.log(data.matches);
 				});
 			} else {
